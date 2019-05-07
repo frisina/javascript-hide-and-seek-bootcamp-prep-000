05f650e84e61620d8ae5c9ab4dcfce3972b2aa47
Marco Frisina <marco@frisina.xyz>
2019-05-07 16:04:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  var x = document.querySelector(selector);
+  return x;
+}
 
+function nestedTarget() {
+
+  var x = document.querySelector('#nested').getElementsByClassName('target')[0];
+  return x;
+
+}
+
+function increaseRankBy(n) {
+
+  var x = document.getElementById('app').querySelectorAll('ul.ranked-list');
+  for (var i = 0; i < x.length; i++) {
+
+    x[i].innerHTML = ((parseInt(x[i].innerHTML)) + n)
+    }
+  }
+
+
+function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
+}
